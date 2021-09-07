5155593eff05f807726ee3c5cebb81aa579978b8
Hernán D'Amico <37642427+hdamico@users.noreply.github.com>
2021-09-07 09:44:12 -0300

    Release renamed to Executor (#4)

--- lib/rails_script_runner.rb
+++ lib/rails_script_runner.rb
@@ -3,6 +3,6 @@ module RailsScriptRunner
   autoload :VERSION, "rails_script_runner/version"
   autoload :Directory, "rails_script_runner/directory"
   autoload :HistoryManager, "rails_script_runner/history_manager"
-  autoload :Release, "rails_script_runner/release"
+  autoload :Executor, "rails_script_runner/executor"
   require "rails_script_runner/railtie" if defined?(Rails)
 end
